fe282a6196744f1448d023fddba8550a4cdee59b
Velibor Markovic <veljko85@users.noreply.github.com>
2017-05-22 17:28:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$(".hamburger").click( function() {
+		
+		$(".druga").fadeToggle();
+		$(".prva").toggleClass("ukrstanje");
+		$(".treca").toggleClass("ukrstanje-suprotno");
+
+		$(".first").toggle( "slide", 600 );
+		$(".second").delay(200).toggle( "slide", 600 );
+		$(".third").delay(400).toggle( "slide", 600 );
+		$(".fourth").delay(600).toggle( "slide", 600 );
+	});
+	
+});
